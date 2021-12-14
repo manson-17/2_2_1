f8aa597f47417fee360598aca398ca2cfaaf97ff
manson17 <cms86@bk.ru>
2021-12-14 11:31:31 +0300

    Done

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,57 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+   @Autowired
+   private SessionFactory sessionFactory;
+
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().saveOrUpdate(user);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> listUsers() {
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
+   }
+
+   @Override
+   public User showUserByCar(String model, int series) {
+
+      Session session = sessionFactory.getCurrentSession();
+
+//      Метод курильщика, тут изначально я получал User по Car. Потом понял, что все равно надо будет переделывать
+//      поолучение по модели и серии для начала получил
+//      Car car = session.createQuery("from Car where model =:carModel and series =:carSeries", Car.class)
+//              .setParameter("carModel", model)
+//              .setParameter("carSeries", series)
+//              .uniqueResult();
+//
+//      User user = session.createQuery("from User where car_id = :carId", User.class)
+//              .setParameter("carId", session.get(Car.class, car.getId()))
+//              .uniqueResult();
+
+
+      User user = session.createQuery("from User user where user.car.model = :carModel and user.car.series = :carSeries", User.class)
+              .setParameter("carModel", model)
+              .setParameter("carSeries", series)
+              .uniqueResult();
+
+
+     return user;
+
+   }
+}
